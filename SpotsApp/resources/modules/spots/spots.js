36213b88ce68900d9f402b73dc949273f3d09683
Thylle <thyssen@mediaworkers.dk>
2017-02-18 16:15:03 +0100

    More accurate wind direction + some styling improvements

--- SpotsApp/resources/modules/spots/spots.js
+++ SpotsApp/resources/modules/spots/spots.js
@@ -58,6 +58,9 @@
                 // This is so that the arrow points in the wind direction instead of against the direction
                 if ($scope.currentSpot.Weather) {
                     $scope.currentSpotWindDegCorrected = (-45 + $scope.currentSpot.Weather.WindDirectionDeg) + 180;
+
+                    //Get the correct optimal wind direction degrese to rotate the icon
+                    spots.calculateDegFromDirection($scope, $scope.currentSpot.OptimalWindDirection);
                 }
 
                 //Remove the old cookie from last time the user checked in - if the count has been reset since.
@@ -101,6 +104,65 @@
                 $scope.$broadcast('scroll.refreshComplete');
                 $scope.$apply();
             }, 1000);
+        },
+
+        calculateDegFromDirection: function($scope, direction) {
+            var degResult = -45;
+            var degOffset = 22.5;
+
+            switch (direction.toUpperCase()) {
+                case "NNE":
+                    degResult = degResult + (degOffset * 1);
+                    break;
+                case "NE":
+                    degResult = degResult + (degOffset * 2);
+                    break;
+                case "ENE":
+                    degResult = degResult + (degOffset * 3);
+                    break;
+                case "E":
+                    degResult = degResult + (degOffset * 4);
+                    break;
+                case "ESE":
+                    degResult = degResult + (degOffset * 5);
+                    break;
+                case "SE":
+                    degResult = degResult + (degOffset * 6);
+                    break;
+                case "SSE":
+                    degResult = degResult + (degOffset * 7);
+                    break;
+                case "S":
+                    degResult = degResult + (degOffset * 8);
+                    break;
+                case "SSW":
+                    degResult = degResult + (degOffset * 9);
+                    break;
+                case "SW":
+                    degResult = degResult + (degOffset * 10);
+                    break;
+                case "WSW":
+                    degResult = degResult + (degOffset * 11);
+                    break;
+                case "W":
+                    degResult = degResult + (degOffset * 12);
+                    break;
+                case "WNW":
+                    degResult = degResult + (degOffset * 13);
+                    break;
+                case "NW":
+                    degResult = degResult + (degOffset * 14);
+                    break;
+                case "NNW":
+                    degResult = degResult + (degOffset * 15);
+                    break;
+            }
+
+
+            $scope.currentSpotOptimalWindDeg = degResult + 180;
+
+            console.log("Direction", direction);
+            console.log("currentSpotOptimalWindDeg", degResult + 180);
         }
     }
 
